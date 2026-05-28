d5da63b7952698de574f747b3602cc29c9e54c27
sarvesh0427 <kshetrysarvesh@gmail.com>
2026-05-28 16:27:45 +0545

    remaining sensors data in dummy sample data

--- src/predict.py
+++ src/predict.py
@@ -30,25 +30,32 @@ if __name__ == "__main__":
 
     # Example dummy input
     sample_data = pd.DataFrame([{
-        "op_setting_1": 0.0,
-        "op_setting_2": 0.0,
-        "op_setting_3": 100.0,
 
-        "sensor_2": 641.82,
-        "sensor_3": 1589.70,
-        "sensor_4": 1400.60,
-        "sensor_7": 554.36,
-        "sensor_8": 2388.06,
-        "sensor_9": 9046.19,
-        "sensor_11": 47.47,
-        "sensor_12": 521.66,
-        "sensor_13": 2388.02,
-        "sensor_14": 8138.62,
-        "sensor_15": 8.4195,
-        "sensor_17": 392,
-        "sensor_20": 39.06,
-        "sensor_21": 23.4190
-    }])
+    "op_setting_1": 0.0,
+    "op_setting_2": 0.0,
+    "op_setting_3": 100.0,
+
+    "sensor_1": 518.67,
+    "sensor_2": 641.82,
+    "sensor_3": 1589.70,
+    "sensor_4": 1400.60,
+    "sensor_5": 14.62,
+    "sensor_6": 21.61,
+    "sensor_7": 554.36,
+    "sensor_8": 2388.06,
+    "sensor_9": 9046.19,
+    "sensor_10": 1.30,
+    "sensor_11": 47.47,
+    "sensor_12": 521.66,
+    "sensor_13": 2388.02,
+    "sensor_14": 8138.62,
+    "sensor_15": 8.4195,
+    "sensor_16": 0.03,
+    "sensor_17": 392.0,
+    "sensor_18": 2388.0,
+    "sensor_19": 100.0,
+    "sensor_20": 39.06,
+    "sensor_21": 23.4190}])
 
     prediction = predict_rul(sample_data)
 
